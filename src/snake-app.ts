22cda6a84d0872c545ec3935beb160d539d4a63d
Alireza Mirian <alireza.mirian@gmail.com>
2019-01-14 01:08:09 +0330

    feat: implement imperative solution

--- src/snake-app.ts
+++ src/snake-app.ts
@@ -3,6 +3,47 @@ import {drawSnake} from './snake-game-utils/ui';
 import {Direction, GameState} from './types';
 
 export function bootstrapSnakeApp(appRoot: JQuery) {
+  let state: GameState = {
+    gameOver: false,
+    seed: null,
+    snakeCells: []
+  };
+  let direction: Direction;
+  let timer;
 
+  appRoot[0].addEventListener('keydown', (event) => {
+    if (event.code === 'Enter') {
+      start();
+    }
+  });
+
+  appRoot[0].addEventListener('keydown', (event) => {
+    if (isArrowEvent(event)) {
+      direction = eventCode2Direction[event.code];
+    }
+    event.preventDefault();
+  });
+
+  function tick() {
+    state = move(state.snakeCells, state.seed, direction);
+    drawSnake(appRoot, state);
+    if (state.gameOver) {
+      cleanup();
+    }
+  }
+
+  function start() {
+    cleanup();
+    state = getInitGameState();
+    direction = Direction.Right;
+    timer = setInterval(tick, 100);
+  }
+
+  function cleanup() {
+    if (timer) {
+      clearInterval(timer);
+      timer = null;
+    }
+  }
 
 }
