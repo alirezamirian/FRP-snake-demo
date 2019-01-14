e1d247692dfdb14d2c068942cb320c3c9cd4069d
Alireza Mirian <alireza.mirian@gmail.com>
2019-01-14 21:16:33 +0330

    tehranjs

--- src/snake-app-reactive.ts
+++ src/snake-app-reactive.ts
@@ -1,10 +1,44 @@
 import {fromEvent, interval} from 'rxjs';
 import {watch} from 'rxjs-watcher';
-import {filter, map, tap, withLatestFrom} from 'rxjs/operators';
-import {eventCode2Direction, isArrowEvent} from './snake-game-utils/logic';
+import {filter, map, scan, startWith, switchMap, withLatestFrom} from 'rxjs/operators';
+import {takeWhile} from './rxjs-utils';
+import {eventCode2Direction, getInitGameState, isArrowEvent, move} from './snake-game-utils/logic';
+import {drawSnake} from './snake-game-utils/ui';
+import {Direction, GameState} from './types';
 
 export function bootstrapReactiveSnakeApp(appRoot: JQuery) {
 
 
+  const keyEvent$ = fromEvent<KeyboardEvent>(appRoot[0], 'keydown');
+
+  const start$ = keyEvent$.pipe(filter(event => event.code === 'Enter'));
+
+  const tick$ = interval(100);
+
+  const direction$ = keyEvent$.pipe(
+    filter(isArrowEvent),
+    map(event => eventCode2Direction[event.code]),
+    startWith(Direction.Right),
+  );
+
+  const movement$ = tick$.pipe(
+    withLatestFrom(direction$),
+    map(([tick, direction]) => direction)
+  );
+
+  const state$ = start$.pipe(
+    switchMap(() => {
+      return movement$.pipe(
+        scan((state: GameState, direction: Direction) =>
+          move(state.snakeCells, state.seed, direction), getInitGameState()
+        ),
+        takeWhile((state => !state.gameOver), true)
+      );
+    })
+  );
+
+  state$.subscribe((state) => {
+    drawSnake(appRoot, state);
+  });
 
 }
--- src/snake-app.ts
+++ src/snake-app.ts
@@ -4,5 +4,41 @@ import {Direction, GameState} from './types';
 
 export function bootstrapSnakeApp(appRoot: JQuery) {
 
+  let state: GameState;
+  let direction: Direction = Direction.Right;
+  let intervalId;
 
+  appRoot[0].addEventListener('keydown', (event) => {
+    if (event.code === 'Enter') {
+      start();
+    }
+  });
+
+  appRoot[0].addEventListener('keydown', (event) => {
+    if (isArrowEvent(event)) {
+      direction = eventCode2Direction[event.code];
+    }
+  });
+
+
+  function tick(){
+    state = move(state.snakeCells, state.seed, direction);
+    drawSnake(appRoot, state);
+    if(state.gameOver){
+      clear();
+    }
+  }
+
+  function  start(){
+    clear();
+    state = getInitGameState();
+    direction = Direction.Right;
+    intervalId = setInterval(tick, 100);
+  }
+
+  function clear(){
+    if(intervalId){
+      clearInterval(intervalId);
+    }
+  }
 }
